cca865a8ce50449163101a5ac6b744e8ecbba2a2
unknown <andreacarolinaanaya2019@gmail.com>
2025-11-02 21:00:59 -0500

    pull request de nivelacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './views/shared/menu-component/menu-component';
+import { ScheduleComponent } from './views/shared/schedule-component/schedule-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent,],
+  imports: [RouterOutlet, MenuComponent, ScheduleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
